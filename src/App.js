3b96cc9b9d22fc221b9f37d947445ffe00ec4bad
Pedro Roberto <pedrofuruguem@hotmail.com>
2023-10-01 19:10:27 -0300

    removendo um item

--- src/App.js
+++ src/App.js
@@ -28,6 +28,11 @@ function App() {
     }
   }
 
+  function removeTask(id){
+    setTasks(tasks.filter((task) => task.id !== id))
+    console.log("id da tarefa removida: " + id)
+  }
+
   return (
     <MainContainer>
       <Container>
@@ -43,7 +48,7 @@ function App() {
           <Subtitulo>Tarefas do dia...</Subtitulo>
 
           {tasks.map((task)=> (
-            <Tarefa key={task.id}>
+            <Tarefa key={task.id} onClick={ ()=> removeTask(task.id)}>
             <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M7.49999 9.31826L12.9548 14.7731L14.7731 12.9548L9.31826 7.49998L14.7731 2.04516L12.9548 0.226886L7.49999 5.68171L2.04516 0.226887L0.226888 2.04516L5.68171 7.49999L0.226888 12.9548L2.04516 14.7731L7.49999 9.31826ZM7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
               <path d="M7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0 25"/>
